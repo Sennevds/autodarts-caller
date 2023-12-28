9f75794dd8905e7f9d7ec0b1fb74a52420272b6d
Senne Vande Sompele <senne@tealpartners.com>
2023-12-28 21:39:40 +0100

    test

--- autodarts-caller.py
+++ autodarts-caller.py
@@ -48,7 +48,7 @@ main_directory = os.path.dirname(os.path.realpath(__file__))
 parent_directory = os.path.dirname(main_directory)
 
 
-VERSION = '2.6.0'
+VERSION = '0.0.1'
 
 
 DEFAULT_EMPTY_PATH = ''
